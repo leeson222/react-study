6b12660cef3226e01acd5bdfcf7c6c4d3ae7f557
이송이 <160142187+leeson222@users.noreply.github.com>
2026-01-02 18:31:03 +0900

    sec05 수정

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,7 +1,17 @@
+// JSX 주의사항
+// 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다
 const Main = () => {
+
+    const number = 9;
+    const obj = {a : 1};
     return(
         <main>
             <h1>main</h1>
+            <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
+            {10}<br/>
+            {number}<br/>
+            {obj.a}
         </main>
     );
 };
