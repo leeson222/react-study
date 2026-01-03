926219cfec3b0022f944f9c50e326cb4ae8b4422
leesongyi <song0478@naver.com>
2026-01-03 09:35:55 +0900

    sec05 수정

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,19 +1,30 @@
+import "./Main.css";
+
 // JSX 주의사항
 // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다
 // 2. 숫자, 문자열, 배열 값만 렌더링 된다
+// 3. 모든 태그는 닫혀 있어야 한다
+// 4. 최상위 태그는 반드시 하나여야만 한다
+
 const Main = () => {
 
-    const number = 9;
-    const obj = {a : 1};
-    return(
-        <main>
-            <h1>main</h1>
-            <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-            {10}<br/>
-            {number}<br/>
-            {obj.a}
-        </main>
-    );
+    const user = {
+        name: "Yushi",
+        isLogin: true,
+    };
+
+    if(user.isLogin) {
+        return <div className="logout">로그아웃</div>
+    } else {
+        return <div>로그인</div>
+    };
+
+    // return(
+    //     <>
+    //         {user.isLogin ? (<div>로그아웃</div>) : (<div>로그인</div>)}
+    //     </>
+    // );
+
 };
 
 export default Main;
